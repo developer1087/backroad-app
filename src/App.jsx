59984834f2cbe414122540dde667be583cf6d59e
Avi Israeli <aviv8710@gmail.com>
2024-05-27 16:50:58 +0300

    done componenting

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbarr from "./components/Navbarr";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbarr />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
